8bd6eb3768693f462c889c422650db7a7f4a0899
John Oliveira <jhonoliver567@gmail.com>
2022-03-12 19:12:03 -0300

    bootstrap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,22 @@
 function App() {
   return (
-    <h1>Pagina Principal</h1>
+    <header>
+      <nav className="container">
+        <div className="dsmovie-nav-content">
+          <h1>DSMovie</h1>
+          <a
+            href="https://github.com/devsuperior"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <div className="dsmovie-contact-container">
+              {/* <GithubIcon /> */}
+              <p className="dsmovie-contact-link">/devsuperior</p>
+            </div>
+          </a>
+        </div>
+      </nav>
+    </header>
   );
 }
 

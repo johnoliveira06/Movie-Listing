e872595047b6135262d2595c0a08c95db366eff5
John Oliveira <jhonoliver567@gmail.com>
2022-03-13 13:48:35 -0300

    Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,9 @@
+import Navbar from "./components/navbar";
+
 function App() {
   return (
-    <header>
-      <nav className="container">
-        <div className="dsmovie-nav-content">
-          <h1>DSMovie</h1>
-          <a
-            href="https://github.com/devsuperior"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <div className="dsmovie-contact-container">
-              {/* <GithubIcon /> */}
-              <p className="dsmovie-contact-link">/devsuperior</p>
-            </div>
-          </a>
-        </div>
-      </nav>
-    </header>
+    <Navbar />
+    //alo
   );
 }
 
